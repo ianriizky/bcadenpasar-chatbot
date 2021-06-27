f66249a01f37871ac59da4e6862ad313132c0bca
ianriizky <ian.rizkypratama@gmail.com>
2021-06-28 06:55:48 +0800

    + Update web route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\ConfigurationController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +25,23 @@ Route::view('/', 'welcome');
 Route::middleware('auth:web', 'verified', 'user_is_active')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
+    Route::post('/order/datatable', [OrderController::class, 'datatable'])->name('order.datatable');
+    Route::resource('/order', OrderController::class)->except('show');
+
     Route::post('/user/datatable', [UserController::class, 'datatable'])->name('user.datatable');
-    Route::resource('/user', UserController::class);
+    Route::resource('/user', UserController::class)->except('show');
+
+    Route::post('/branch/datatable', [BranchController::class, 'datatable'])->name('branch.datatable');
+    Route::resource('/branch', BranchController::class)->except('show');
 
     Route::post('/customer/datatable', [CustomerController::class, 'datatable'])->name('customer.datatable');
-    Route::resource('/customer', CustomerController::class);
+    Route::resource('/customer', CustomerController::class)->except('show');
+
+    Route::post('/role/datatable', [RoleController::class, 'datatable'])->name('role.datatable');
+    Route::resource('/role', RoleController::class)->except('show');
+
+    Route::post('/configuration/datatable', [ConfigurationController::class, 'datatable'])->name('configuration.datatable');
+    Route::resource('/configuration', ConfigurationController::class)->except('show');
 });
 
 require __DIR__.'/auth.php';
